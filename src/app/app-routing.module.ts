ef03176236370c10ac5bfe556329725c08e39e1c
gokul <gokulchandan18@gmail.com>
2022-03-21 09:51:04 +0530

    commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,5 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { AuthGuard } from './auth.guard';
-import { Dashboard3Component } from './dashboard3/dashboard3.component';
-import { FileuploadComponent } from './fileupload/fileupload.component';
-import { HomeComponent } from './home/home.component';
-import { LoginComponent } from './login/login.component';
-import { RegisterComponent } from './register/register.component';
 import { SpecialEventsComponent } from './special-events/special-events.component';
 
 
@@ -15,36 +9,14 @@ import { SpecialEventsComponent } from './special-events/special-events.componen
 const routes: Routes = [
     {
         path: '',
-        redirectTo: '/fileupload',
+        redirectTo: '/special',
         pathMatch: 'full'
-      },
-      {
-        path: 'home',
-        component: HomeComponent
-      },
-      {
-        path: 'dashboard3',
-        
-        component: Dashboard3Component
-      },
-      {
-        path: 'special',
-        // canActivate: [AuthGuard],
-        component: SpecialEventsComponent
-      },
-      {
-        path: 'register',
-        component: RegisterComponent
-      },
-      {
-        path: 'login',
-        component: LoginComponent
       },
      
       {
-        path: 'fileupload',
-        
-        component: FileuploadComponent
+        path: 'special',
+       
+        component: SpecialEventsComponent
       }
     ];
 
