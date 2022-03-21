da976272f11a1e4a2d860eede182a91824c26570
gokul <gokulchandan18@gmail.com>
2022-03-21 10:00:39 +0530

    commit

--- src/app/special-events/special-events.component.ts
+++ src/app/special-events/special-events.component.ts
@@ -1,4 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { Items, Item, GithubResponse } from '../interface';
+import { GithubService } from '../services/github.service';
+import { Observable, observable, of } from 'rxjs';
+import { FormControl } from '@angular/forms';
+import {
+  startWith,
+  map,
+  debounceTime,
+  mergeMapTo,
+  mergeMap,
+  switchMap,
+  catchError
+} from 'rxjs/operators';
 
 @Component({
   selector: 'app-special-events',
@@ -6,10 +19,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./special-events.component.css']
 })
 export class SpecialEventsComponent implements OnInit {
+  public githubAutoComplete$: Observable<Items> = null;
+  public autoCompleteControl = new FormControl();
 
-  constructor() { }
+  constructor(private githubService: GithubService) {}
 
-  ngOnInit() {
+  lookup(value: string): Observable<Items> {
+    return this.githubService.search(value.toLowerCase()).pipe(
+      // map the item property of the github results as our return object
+      map(results => results.items),
+      // catch errors
+      catchError(_ => {
+        return of(null);
+      })
+    );
   }
 
+  ngOnInit() {
+    this.githubAutoComplete$ = this.autoCompleteControl.valueChanges.pipe(
+      startWith(''),
+      // delay emits
+      debounceTime(300),
+      // use switch map so as to cancel previous subscribed events, before creating new once
+      switchMap(value => {
+        if (value !== '') {
+          // lookup from github
+          return this.lookup(value);
+        } else {
+          // if no value is pressent, return null
+          return of(null);
+        }
+      })
+    );
+  }
 }
